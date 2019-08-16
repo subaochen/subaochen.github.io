05e33fc0b2758a28731c872827b3a1c94d63418e
subaochen <subaochen@126.com>
2019-08-16 11:33:45 +0800

    grid word example

---
+++ resources/grid_world.py
@@ -0,0 +1,117 @@
+#######################################################################
+# Copyright (C)                                                       #
+# 2016-2018 Shangtong Zhang(zhangshangtong.cpp@gmail.com)             #
+# 2016 Kenta Shimada(hyperkentakun@gmail.com)                         #
+# Permission given to modify the code as long as you keep this        #
+# declaration at the top                                              #
+#######################################################################
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.table import Table
+
+matplotlib.use('Agg')
+
+WORLD_SIZE = 5
+A_POS = [0, 1]
+A_PRIME_POS = [4, 1]
+B_POS = [0, 3]
+B_PRIME_POS = [2, 3]
+DISCOUNT = 0.9
+
+# 把动作定义为对x，y坐标的增减改变
+ACTIONS = [np.array([0, -1]), # up
+           np.array([-1, 0]), # left
+           np.array([0, 1]),  # down
+           np.array([1, 0])]  # right
+ACTION_PROB = 0.25
+
+
+def step(state, action):
+    """每次走一步
+    :param state:当前状态，坐标的list，比如[1,1]
+    :apram action:当前采取的动作，是对状态坐标的修正
+    :return:下一个状态（坐标的list）和reward
+    """
+    if state == A_POS:
+        return A_PRIME_POS, 10
+    if state == B_POS:
+        return B_PRIME_POS, 5
+
+    next_state = (np.array(state) + action).tolist()
+    x, y = next_state
+    if x < 0 or x >= WORLD_SIZE or y < 0 or y >= WORLD_SIZE:
+        reward = -1.0
+        next_state = state
+    else:
+        reward = 0
+    return next_state, reward
+
+
+def draw_image(image):
+    fig, ax = plt.subplots()
+    ax.set_axis_off()
+    tb = Table(ax, bbox=[0, 0, 1, 1])
+
+    nrows, ncols = image.shape
+    width, height = 1.0 / ncols, 1.0 / nrows
+
+    # Add cells
+    for (i, j), val in np.ndenumerate(image):
+        tb.add_cell(i, j, width, height, text=val,
+                    loc='center', facecolor='white')
+
+    # Row and column labels...
+    for i in range(len(image)):
+        tb.add_cell(i, -1, width, height, text=i+1, loc='right',
+                    edgecolor='none', facecolor='none')
+        tb.add_cell(-1, i, width, height/2, text=i+1, loc='center',
+                    edgecolor='none', facecolor='none')
+
+    ax.add_table(tb)
+
+
+def figure_3_2():
+    value = np.zeros((WORLD_SIZE, WORLD_SIZE))
+    while True:
+        # keep iteration until convergence
+        new_value = np.zeros_like(value)
+        for i in range(WORLD_SIZE):
+            for j in range(WORLD_SIZE):
+                for action in ACTIONS:
+                    (next_i, next_j), reward = step([i, j], action)
+                    # bellman equation
+                    new_value[i, j] += ACTION_PROB * (reward + DISCOUNT * value[next_i, next_j])
+        if np.sum(np.abs(value - new_value)) < 1e-4:
+            draw_image(np.round(new_value, decimals=2))
+            plt.savefig('../images/rl/mdp/figure_3_2.png')
+            plt.close()
+            break
+        value = new_value
+
+
+def figure_3_5():
+    value = np.zeros((WORLD_SIZE, WORLD_SIZE))
+    while True:
+        # keep iteration until convergence
+        new_value = np.zeros_like(value)
+        for i in range(WORLD_SIZE):
+            for j in range(WORLD_SIZE):
+                values = []
+                for action in ACTIONS:
+                    (next_i, next_j), reward = step([i, j], action)
+                    # value iteration
+                    values.append(reward + DISCOUNT * value[next_i, next_j])
+                new_value[i, j] = np.max(values)
+        if np.sum(np.abs(new_value - value)) < 1e-4:
+            draw_image(np.round(new_value, decimals=2))
+            plt.savefig('../images/rl/mdp/figure_3_5.png')
+            plt.close()
+            break
+        value = new_value
+
+
+if __name__ == '__main__':
+    figure_3_2()
+    figure_3_5()
